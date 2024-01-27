4119b6bd6d05f121bb3da1c0ac8ee5456fd69445
yudyagilev <yu.dyagilev@gmail.com>
2024-01-28 00:44:22 +0200

    fix

--- src/tests/simple_request.spec.ts
+++ src/tests/simple_request.spec.ts
@@ -1,33 +1,64 @@
 import axios from 'axios';
 
+const BASE_URL:string = "https://httpbin.org"
+
 describe('Check API requests to HttpBin', () => {
     test('Check POST request', async () => {
-        const response = await axios.post('https://httpbin.org/post', {
-            hello: 'world'
-        });
+        const requestBody = {
+            yurii: 'hello!'
+        }
 
+        const response = await axios.post(`${BASE_URL}/post`, requestBody);
+
+        expect(response.status).toEqual(200);
         expect(response.data.args).toEqual({});
         expect(response.data.files).toEqual({});
         expect(response.data.form).toEqual({});
-        expect(response.data.json).toEqual({ hello: 'world' });
-        expect(response.data.data).toEqual('{"hello":"world"}');
+        expect(response.data.json).toEqual(requestBody);
+        expect(response.data.data).toEqual(JSON.stringify(requestBody));
     });
 
     test('Check GET request', async () => {
-        const response = await axios.get('https://httpbin.org/get');
+        const response = await axios.get(`${BASE_URL}/get`);
 
+        expect(response.status).toEqual(200);
         expect(response.data.args).toEqual({});
-        expect(response.data.url).toEqual('https://httpbin.org/get');
+        expect(response.data.url).toEqual(`${BASE_URL}/get`);
     
     });
 
     test('Check PUT request', async () => {
-        const response = await axios.put('https://httpbin.org/put', 'changedHello=changedWorld');
+        const requestBody = {
+            changedHello: 'changedWorld'
+        }
+        const response = await axios.put(`${BASE_URL}/put`, requestBody);
 
-        expect(response.data.form).toEqual({ changedHello: 'changedWorld' });
+        expect(response.status).toEqual(200);
+        expect(response.data.data).toEqual(JSON.stringify(requestBody));
         expect(response.data.args).toEqual({});
         expect(response.data.files).toEqual({});
-        expect(response.data.data).toBe('');
-        expect(response.data.json).toBe(null);
+        expect(response.data.json).toEqual(requestBody);
     });
+    
+
+    test('Check PATCH request', async () => {
+        const sampleUpdate = {
+            propertyToUpdate: 'newValue'
+        };
+
+        const response = await axios.patch(`${BASE_URL}/patch`, sampleUpdate);
+
+        expect(response.status).toEqual(200);
+        expect(response.data.url).toContain('httpbin.org/patch');
+        expect(response.data.json).toEqual(sampleUpdate);
+    });
+
+    test('Check DELETE request', async () => {
+        const response = await axios.delete(`${BASE_URL}/delete`, {
+            data: {sampleData: 'test'}
+        })
+
+        expect(response.status).toEqual(200);
+        expect(response.data.url).toContain('httpbin.org/delete');
+    })
 });
